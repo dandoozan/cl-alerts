8031c109b0d18c17f5d51ed479891ff2e8ce8940
Dan Doozan <dandoozan@gmail.com>
2019-10-05 08:33:20 -0700

    Add Header component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styles from './App.module.css';
+import Header from '../Header';
 
 function App() {
-  return <div className={styles.app}>App</div>;
+  return (
+    <div className={styles.app}>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+interface Props {}
+
+export default function Header({ }: Props) {
+    return (<div className={styles.header}>Header</div>);
+};
