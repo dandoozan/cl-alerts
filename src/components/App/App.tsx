a3adf3afdd87ef861cd74a52e78bdc6aa01bc9fe
Dan Doozan <dandoozan@gmail.com>
2019-10-05 12:07:07 -0700

    Add Form component and do basic layout

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './App.module.css';
 import Header from '../Header';
+import Form from '../Form';
 
 function App() {
   return (
     <div className={styles.app}>
       <Header />
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Form.module.css';
+
+interface Props {}
+
+export default function Form({  }: Props) {
+  return <div className={styles.form}>Form</div>;
+}
