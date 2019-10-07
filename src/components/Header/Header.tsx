382c5f7bb5c13dbcb73cff8daf0395ed900978b4
Dan Doozan <dandoozan@gmail.com>
2019-10-06 20:03:41 -0700

    Add preliminary form fields

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,5 +4,12 @@ import styles from './Header.module.css';
 interface Props {}
 
 export default function Header({  }: Props) {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles.title}>Craigslist Alerts</div>
+      <div className={styles.subtitle}>
+        Automatically receive craigslist results via email.
+      </div>
+    </header>
+  );
 }
