e419d57a41fc56228c71aed7a7b279ece7a8ad23
Dan Doozan <dandoozan@gmail.com>
2019-10-09 19:02:27 -0700

    Add Firebase

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -2,11 +2,37 @@ import React, { useState } from 'react';
 import styles from './HomePage.module.css';
 import { Form, Row, Col, Button } from 'react-bootstrap';
 import { Redirect } from 'react-router-dom';
+import firebase from '../../firebase';
 
 interface Props {}
 
 export default function HomePage({  }: Props) {
   let [toCreate, setToCreate] = useState(false);
+  let [city, setCity] = useState('Phoenix');
+  let [searchTerm, setSearchTerm] = useState('');
+  let [email, setEmail] = useState('');
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    console.log(`***onsubmit`);
+    console.log('​city=', city);
+    console.log('​searchTerm=', searchTerm);
+    console.log('​email=', email);
+
+    //store these in firebase
+    try {
+      let docRef = await firebase.firestore().collection('alerts').add({
+        city,
+        searchTerm,
+        email,
+      });
+      console.log('Document written with ID: ', docRef.id);
+    } catch (error) {
+      console.error('Error adding document: ', error);
+    }
+    //setToCreate(true)
+  }
 
   if (toCreate) {
     return <Redirect to="/create" />;
@@ -14,18 +40,20 @@ export default function HomePage({  }: Props) {
     return (
       <div className={styles.homePage}>
         <h2 className="text-center">Search Craigslist</h2>
-        <Form onSubmit={() => setToCreate(true)}>
+        <Form onSubmit={handleSubmit}>
           <Form.Group as={Row} controlId="searchForm.city">
             <Form.Label column sm="2">
               City
             </Form.Label>
             <Col sm="10">
-              <Form.Control as="select">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
+              <Form.Control
+                as="select"
+                value={city}
+                //@ts-ignore (ignore because typescript complains about "value")
+                onChange={e => setCity(e.target.value)}
+              >
+                <option>Phoenix</option>
+                <option>Tucson</option>
               </Form.Control>
             </Col>
           </Form.Group>
@@ -35,7 +63,11 @@ export default function HomePage({  }: Props) {
               Search Term
             </Form.Label>
             <Col sm="10">
-              <Form.Control placeholder="(Optional) e.g. dining table" />
+              <Form.Control
+                placeholder="(Optional) e.g. dining table"
+                value={searchTerm}
+                onChange={e => setSearchTerm(e.target.value)}
+              />
             </Col>
           </Form.Group>
 
@@ -44,7 +76,12 @@ export default function HomePage({  }: Props) {
               Email
             </Form.Label>
             <Col sm="10">
-              <Form.Control type="email" placeholder="e.g. johndoe@gmail.com" />
+              <Form.Control
+                type="email"
+                placeholder="e.g. johndoe@gmail.com"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+              />
             </Col>
           </Form.Group>
 
